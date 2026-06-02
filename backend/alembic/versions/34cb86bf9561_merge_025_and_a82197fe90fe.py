361e883821bc40be104f6e5ffd0a1ece137b4525
honguyenhung <honguyennhung2010@gmail.com>
2026-06-02 16:09:28 +0700

    fix: recreate merge migration

---
+++ backend/alembic/versions/34cb86bf9561_merge_025_and_a82197fe90fe.py
@@ -0,0 +1,24 @@
+"""Merge 025 and a82197fe90fe
+
+Revision ID: 34cb86bf9561
+Revises: 025, a82197fe90fe
+Create Date: 2026-06-02 16:09:17.438437
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '34cb86bf9561'
+down_revision = ('025', 'a82197fe90fe')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
