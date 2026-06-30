273307069a0bac066be0060e1b51658ffc0ad38e
Nope <honguyenhung29@gmail.com>
2026-06-30 21:20:35 +0700

    fix: merge alembic multiple heads to allow migrations to run (#91)
    Co-authored-by: honguyenhung <honguyennhung2010@gmail.com>

---
+++ backend/alembic/versions/914d78ba6c8e_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 914d78ba6c8e
+Revises: a1b2c3d4e5f6, 7a8e2eb4683b
+Create Date: 2026-06-30 21:18:26.978785
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '914d78ba6c8e'
+down_revision = ('a1b2c3d4e5f6', '7a8e2eb4683b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
